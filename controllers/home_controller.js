eb1d75833c8a118372c2606660ac6ac63cf99a84
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-04-11 22:01:42 +0530

    create a controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+//actions
+
+module.exports.home = function(req,res){
+    return res.end('<h1>express is set for firm</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
 
+const homeContorller=require('../controllers/home_controller');
+
 //console.log(`router loaded`);
 
+router.get('/', homeContorller.home);
 module.exports=router;
