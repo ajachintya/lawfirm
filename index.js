68282f9ba2e3ce89bf0bc60c29fd8ae13a388ca9
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-04-12 12:56:45 +0530

    installed ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port=8080;
 //use express router
 app.use('/',require('./routes/index'));
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 app.listen(port , function(err){
     if(err){
