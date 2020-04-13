7b44877b1b979603ecc76867152e00a96369b0d1
deepsingh11421 <deepsingh1121@gmail.com>
2020-04-13 14:18:04 +0530

    Check

---
+++ lawfirm/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app=express();
+const port=8080;
+
+const db=require('./config/mongoose');
+//Check
+//use express router
+app.use('/',require('./routes/index'));
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//middleware to use static files like .css
+app.use(express.static('assets'));
+
+app.listen(port , function(err){
+    if(err){
+        console.log(`Error in running the server ${err}`);
+    }
+    console.log(`Server is running on port ${port}`);
+});
